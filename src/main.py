bc200161cca3aefb751de59ee85de1c99a451e1c
nick <nick@kisanhub.com>
2022-11-21 15:28:14 +0000

    First cut at Python runner - result collection is next (and I need to decide whether to push directly to main or to the hub)

--- src/main.py
+++ src/main.py
@@ -13,16 +13,17 @@ from common.schemas import TestRun
 HUB_POLL_PERIOD = int(os.environ.get('HUB_POLL_PERIOD', 10))
 
 
-class BuildCancelled(Exception):
-    pass
+class BuildFailed(Exception):
+    def __init__(self, reason=False):
+        self.reason = reason
 
 
 def init_build_dirs():
     shutil.rmtree('build', ignore_errors=True)
     if os.path.exists('dist.tgz'):
         os.remove('dist.tgz')
-    os.makedirs('build/cypress/results/json')
-    os.makedirs('build/cypress/screenshots')
+    os.makedirs('build/cykube/results/json')
+    os.makedirs('build/cykube/results/screenshots')
 
 
 def fetch_dist(id: int, sha: str, timeout: int, hub_url: str, cache_url: str) -> TestRun:
@@ -41,7 +42,7 @@ def fetch_dist(id: int, sha: str, timeout: int, hub_url: str, cache_url: str) ->
         r = requests.get(f'{hub_url}/{id}')
         if r.status_code != 200:
             logging.error(f"Failed to contact hub: {r.status_code}")
-            raise BuildCancelled()
+            raise BuildFailed()
 
         testrun = TestRun(**r.json())
         status = testrun.status
@@ -59,21 +60,58 @@ def fetch_dist(id: int, sha: str, timeout: int, hub_url: str, cache_url: str) ->
             return testrun
         elif status != 'building':
             logging.info("Build failed or cancelled: quit")
-            raise BuildCancelled()
+            raise BuildFailed()
 
         # otherwise sleep
         sleep(HUB_POLL_PERIOD)
 
     logging.info("Reached timeout - quitting")
-    raise BuildCancelled()
+    raise BuildFailed()
 
 
-def start_server():
+def start_server(testrun: TestRun) -> subprocess.Popen:
     """
     Start the server
     :return:
     """
+    proc = subprocess.Popen([testrun.server_cmd.split(' ')], shell=True)
+    # wait until it's ready
+    endtime = time() + 30
+    logging.info("Waiting for server to be ready...")
+    while True:
+        r = requests.get(f'http://localhost:{testrun.server_port}')
+        if r.status_code != 200:
+            if time() > endtime:
+                logging.error('Failed to start server')
+                raise BuildFailed()
+            sleep(5)
+        else:
+            return proc
 
+
+def run_tests(testrun: TestRun, hub_url: str, timeout: int):
+    while True:
+        r = requests.get(f'{hub_url}/testrun/{testrun.id}/next')
+        if r.status_code == 204:
+            # we're done
+            return
+        elif r.status_code == 200:
+            # run the test
+            spec = r.json()
+            try:
+                subprocess.run(['../node_modules/.bin/cypress', 'run', '-s', spec['file'],
+                                '--reporter=../json-reporter.js',
+                                '-o', 'output=cykube/results/json',
+                                '-c', 'screenshotsFolder=cykube/results/screenshots,screenshotOnRunFailure=true'],
+                               timeout=timeout, check=True)
+                # TODO report report to hub and cleanup
+
+            except subprocess.CalledProcessError as ex:
+                raise BuildFailed(f'Cypress run failed with return code {ex.returncode}')
+            except subprocess.TimeoutExpired:
+                raise BuildFailed("Exceeded run timeout")
+        else:
+            raise BuildFailed(f"Received unexpected status code from hub: {r.status_code}")
 
 
 def main():
@@ -90,9 +128,20 @@ def main():
 
     args = parser.parse_args()
     # fetch dist first
-    fetch_dist(args.id, args.sha, args.timeout, args.hub, args.cache)
-    # now start the server
-    start_server()
+    testrun = fetch_dist(args.id, args.sha, args.timeout, args.hub, args.cache)
+    # start the server
+    proc = start_server(testrun)
+    try:
+        # now fetch specs until we're done or the build is cancelled
+        run_tests(testrun, args.hub, args.timeout)
+    except BuildFailed as ex:
+        # TODO inform the hub
+        logging.error(ex.reason)
+    finally:
+        # kill the server
+        proc.kill()
+
+    sys.exit(0)
 
 
 if __name__ == '__main__':
--- src/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# default build timeout, in seconds
-BUILD_TIMEOUT: int = os.environ.get('BUILD_TIMEOUT', 30*60)
-
