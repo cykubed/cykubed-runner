968b7eb8fcc8104352a57b64b70063856cf0239c
nick <nickbrook72@gmail.com>
2023-02-28 23:48:59 +0000

    latest runner tag

--- scripts/runner-images.sql
+++ scripts/runner-images.sql
@@ -1,4 +1,4 @@
 delete from runnerimage;
-insert into runnerimage (node_version, tag, description) values ('16.13.0', "nickbrookck/cykube-runner:node16.13-46ef124", 'Node 16.13');
-insert into runnerimage (node_version, tag, description) values ('16.x', 'nickbrookck/cykube-runner:node16.x-46ef124',  'Node 16.x');
+insert into runnerimage (node_version, tag, description) values ('16.13.0', "nickbrookck/cykube-runner:node16.13-31340cc", 'Node 16.13');
+insert into runnerimage (node_version, tag, description) values ('16.x', 'nickbrookck/cykube-runner:node16.x-31340cc',  'Node 16.x');
 ##
