31340cc7a91ef789f02f017f9023cfc5325b3388
nick <nickbrook72@gmail.com>
2023-02-28 18:19:21 +0000

    Added google-cloud-logging

--- scripts/runner-images.sql
+++ scripts/runner-images.sql
@@ -1,4 +1,4 @@
 delete from runnerimage;
-insert into runnerimage (node_version, tag, description) values ('16.13.0', "nickbrookck/cykube-runner:node16.13-7d71949", 'Node 16.13');
-insert into runnerimage (node_version, tag, description) values ('16.x', 'nickbrookck/cykube-runner:node16.x-7d71949',  'Node 16.x');
+insert into runnerimage (node_version, tag, description) values ('16.13.0', "nickbrookck/cykube-runner:node16.13-46ef124", 'Node 16.13');
+insert into runnerimage (node_version, tag, description) values ('16.x', 'nickbrookck/cykube-runner:node16.x-46ef124',  'Node 16.x');
 ##
