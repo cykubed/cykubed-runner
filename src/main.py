04dcd3e7e67bfecdc9b1133636fcd4cf1b8d2dc2
nick <nickbrook72@gmail.com>
2023-03-15 12:32:49 +0000

    Revamped to use asyncio and Redis - untested

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ import argparse
 import asyncio
 import os
 import sys
-from time import sleep
 
 import sentry_sdk
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
 from sentry_sdk.integrations.httpx import HttpxIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 import build
 import cypress
@@ -24,34 +24,30 @@ def handle_sigterm_builder(signum, frame):
 def main():
     parser = argparse.ArgumentParser('CykubeRunner')
     parser.add_argument('--loglevel', default='info', help='Log level')
-    parser.add_argument('command', choices=['shell', 'build', 'run'], help='Command')
+    parser.add_argument('command', choices=['build', 'run'], help='Command')
     parser.add_argument('testrun_id', type=int, help='Test run ID')
 
     args = parser.parse_args()
 
     if os.environ.get('SENTRY_DSN'):
         sentry_sdk.init(integrations=[
+            RedisIntegration(),
             HttpxIntegration(),
             AsyncioIntegration(),
         ], )
 
     cmd = args.command
+    # we'll need access to Redis
+    try:
+        redis()
+    except Exception as ex:
+        logger.error(f"Failed to contact Redis ({ex}) - quitting")
+        sys.exit(1)
 
-    if cmd == 'shell':
-        sleep(3600*24)
-        sys.exit(0)
+    if cmd == 'build':
+        asyncio.run(build.run(args.testrun_id))
     else:
-        # we'll need access to Redis
-        try:
-            redis()
-        except Exception as ex:
-            logger.error(f"Failed to contact Redis ({ex}) - quitting")
-            sys.exit(1)
-
-        if cmd == 'build':
-            asyncio.run(build.run(args.testrun_id))
-        else:
-            asyncio.run(cypress.run(args.testrun_id))
+        asyncio.run(cypress.run(args.testrun_id))
 
 
 if __name__ == '__main__':
