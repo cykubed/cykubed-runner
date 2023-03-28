b0066401ab9366ee9ccec9a8ca5900173ddd68d2
nick <nickbrook72@gmail.com>
2023-03-28 11:46:39 +0100

    Typo

--- src/cypress.py
+++ src/cypress.py
@@ -89,7 +89,7 @@ def parse_results(started_at: datetime.datetime) -> SpecResult:
                                                    message=err['message'],
                                                    stack=err['stack'],
                                                    code_frame=CodeFrame(line=frame['line'],
-                                                                        file=fname['relativeFile'],
+                                                                        file=frame['relativeFile'],
                                                                         column=frame['column'],
                                                                         language=frame['language'],
                                                                         frame=frame['frame']))
