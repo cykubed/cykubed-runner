284e2638d985fd854bb50e1660da33d9dd6d67d9
nick <nickbrook72@gmail.com>
2023-04-01 20:06:31 +0100

    Enable stackdriver logging

--- src/server.py
+++ src/server.py
@@ -83,8 +83,7 @@ class SPAHandler(SimpleHTTPRequestHandler):
             self.send_response(HTTPStatus.OK)
             self.send_header("Content-type", ctype)
             self.send_header("Content-Length", str(fs[6]))
-            self.send_header("Last-Modified",
-                self.date_time_string(fs.st_mtime))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
             self.end_headers()
             return f
         except:
