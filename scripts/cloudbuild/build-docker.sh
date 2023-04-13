42d352bb3e70bf5a36a905cfd2484923f4477295
nick <nickbrook72@gmail.com>
2023-04-13 15:24:06 +0100

    Add caching

--- scripts/cloudbuild/build-docker.sh
+++ scripts/cloudbuild/build-docker.sh
@@ -2,11 +2,13 @@ VERSION=$(cat /workspace/version.txt)
 
 docker build . -f dockerfiles/base/Dockerfile \
                 -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:"$VERSION" \
-                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:latest
+                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:latest \
+                --cache-from europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:latest
 
 docker build . -f dockerfiles/node16.x/Dockerfile \
                 -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:"$VERSION" \
                 -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:latest \
+                --cache-from europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:latest \
                 --build-arg TAG="$VERSION"
 
 sed "s/IMAGE_TAG/$VERSION/g" scripts/cloudbuild/payload.json > /workspace/cykube-payload.json
