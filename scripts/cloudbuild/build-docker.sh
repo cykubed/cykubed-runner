baf4ca32d068aebc6c4a99847f0898f54f1eaac1
nick <nickbrook72@gmail.com>
2023-04-13 15:16:40 +0100

    Back to non-root user

--- scripts/cloudbuild/build-docker.sh
+++ scripts/cloudbuild/build-docker.sh
@@ -1,6 +1,14 @@
 VERSION=$(cat /workspace/version.txt)
-docker build . -f dockerfiles/base/Dockerfile -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:"$VERSION"
-docker build . -f dockerfiles/node16.x/Dockerfile -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:"$VERSION" --build-arg TAG="$VERSION"
+
+docker build . -f dockerfiles/base/Dockerfile \
+                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:"$VERSION" \
+                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:latest
+
+docker build . -f dockerfiles/node16.x/Dockerfile \
+                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:"$VERSION" \
+                -t europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:latest \
+                --build-arg TAG="$VERSION"
+
 sed "s/IMAGE_TAG/$VERSION/g" scripts/cloudbuild/payload.json > /workspace/cykube-payload.json
 echo "{\"text\":\"Cykube runner $VERSION published\"}" > /workspace/slack-payload.json
 
--- scripts/cloudbuild/push-docker.sh
+++ scripts/cloudbuild/push-docker.sh
@@ -1,4 +1,3 @@
-VERSION=$(cat /workspace/version.txt)
-docker push europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:"$VERSION"
-docker push europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:"$VERSION"
+docker push -a europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base
+docker push -a europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x
 
