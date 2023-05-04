1ffa4e4a82573e100ff5994a7effb2b69f299f12
nick <nickbrook72@gmail.com>
2023-05-04 18:07:19 +0100

    Add Chrome to the image

--- scripts/build-minikube.sh
+++ scripts/build-minikube.sh
@@ -1,5 +1,5 @@
-IMAGE="europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:$1"
-docker build . -f dockerfiles/base/Dockerfile -t "europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-base:$1"
+IMAGE="europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:$1"
+docker build . -f dockerfiles/base/Dockerfile -t "europe-docker.pkg.dev/cykubeapp/cykubed/runner-base:$1"
 docker build . -f dockerfiles/node16.x/Dockerfile -t "$IMAGE" --build-arg TAG="$1"
 minikube image load "$IMAGE"
 echo "Loaded image $IMAGE"
--- scripts/runner-images.sql
+++ scripts/runner-images.sql
@@ -1,6 +1,6 @@
 delete from runnerimage;
 insert into runnerimage (node_version, tag, description) values
-                         ('16.x', 'europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:TAG',  'Node 16.x');
-update project set runner_image = 'europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:TAG' where runner_image like 'europe-west2-docker.pkg.dev/cykubeapp/cykube/runner-node16.x:%';
+                         ('16.x', 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:TAG',  'Node 16.x');
+update project set runner_image = 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:TAG' where runner_image like 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:%';
 
 
