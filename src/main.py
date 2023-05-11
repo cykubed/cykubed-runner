0287fc9ef8fb27d6662d8ea5407dc768a93d4998
nick <nickbrook72@gmail.com>
2023-05-11 20:20:25 +0100

    sleep failed pods

--- src/main.py
+++ src/main.py
@@ -82,6 +82,7 @@ def main():
         # tell the agent
         send_agent_event(AgentEvent(type=AgentEventType.run_completed,
                                     testrun_id=trid))
+        time.sleep(3600)
         sys.exit(0)
     except Exception as ex:
         logger.exception(f'Build failed: {ex}')
