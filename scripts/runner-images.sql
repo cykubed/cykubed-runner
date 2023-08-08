52720e47587b0c8f758565928a4e2649b47a2f1c
nick <nickbrook72@gmail.com>
2023-08-08 19:20:12 +0100

    New API token

--- scripts/runner-images.sql
+++ scripts/runner-images.sql
@@ -1,5 +1,5 @@
-delete from runnerimage;
-insert into runnerimage (node_version, tag, description) values
+delete from runner_image;
+insert into runner_image (node_version, tag, description) values
                          ('16.x', 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:TAG',  'Node 16.x');
 update project set runner_image = 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:TAG' where runner_image like 'europe-docker.pkg.dev/cykubeapp/cykubed/runner-node16.x:%';
 
