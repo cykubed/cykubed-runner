db9b3b612d54037f7af09fd8dc26f37ea771d854
nick <nickbrook72@gmail.com>
2023-09-15 10:18:15 +0100

    Test

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.6.0 -f dockerfiles/generated/full/node-14/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.7.0 -f dockerfiles/generated/full/node-14/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14
-docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.6.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.7.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome
-docker build -t us-docker.pkg.dev/cykubed/public/node-14-edge:3.6.0 -f dockerfiles/generated/full/node-14-edge/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14-edge:3.7.0 -f dockerfiles/generated/full/node-14-edge/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14-edge
-docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome-edge-firefox:3.6.0 -f dockerfiles/generated/full/node-14-chrome-edge-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome-edge-firefox:3.7.0 -f dockerfiles/generated/full/node-14-chrome-edge-firefox/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome-edge-firefox
 
 http POST https://api.cykubed.com/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
