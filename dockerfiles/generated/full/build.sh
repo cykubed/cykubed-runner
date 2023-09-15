eeac56cf31b1ad91d0130b6a4cf878f93a90ebfc
nick <nickbrook72@gmail.com>
2023-09-15 08:54:23 +0100

    Added push to shell build script

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.0.1 -f dockerfiles/generated/full/node-14/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14
 docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.0.1 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome
 docker build -t us-docker.pkg.dev/cykubed/public/node-14-edge:3.0.1 -f dockerfiles/generated/full/node-14-edge/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14-edge
 docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome-edge-firefox:3.0.1 -f dockerfiles/generated/full/node-14-chrome-edge-firefox/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome-edge-firefox
 
 http POST http://localhost:5002/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
 http POST $SLACK_HOOK_URL < dockerfiles/generated/full/slack-payload.json
--- dockerfiles/templates/full/shell-step.tpl
+++ dockerfiles/templates/full/shell-step.tpl
@@ -1,1 +1,2 @@
 docker build -t $region-docker.pkg.dev/cykubed/public/$path:$tag -f dockerfiles/generated/full/$path/Dockerfile .
+docker push -a $region-docker.pkg.dev/cykubed/public/$path
