6ddfea88db7f074c14821265506d1117236f7afe
nick <nickbrook72@gmail.com>
2023-09-15 17:52:11 +0100

    Faster build

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.13.0 -f dockerfiles/base/runner/Dockerfile .
-#docker push -a us-docker.pkg.dev/cykubed/public/base-runner
-docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.13.0 -f dockerfiles/generated/full/node-14/Dockerfile .
-#docker push -a us-docker.pkg.dev/cykubed/public/node-14
-docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.13.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
-#docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome
+docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.14.0 -f dockerfiles/base/runner/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/base-runner
+docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.14.0 -f dockerfiles/generated/full/node-14/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14
+docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.14.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
+docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome
 
-#http POST https://api.cykubed.com/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
-#http POST $SLACK_HOOK_URL < dockerfiles/generated/full/slack-payload.json
+http POST https://api.cykubed.com/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
+http POST $SLACK_HOOK_URL < dockerfiles/generated/full/slack-payload.json
