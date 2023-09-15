c6b9255e90592342cea66917ea1ecbef6e1769e5
nick <nickbrook72@gmail.com>
2023-09-15 19:03:18 +0100

    Fixed submodule issue

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.16.0 -f dockerfiles/base/runner/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.17.0 -f dockerfiles/base/runner/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/base-runner
-docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.16.0 -f dockerfiles/generated/full/node-14/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14:3.17.0 -f dockerfiles/generated/full/node-14/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14
-docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.16.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-14-chrome:3.17.0 -f dockerfiles/generated/full/node-14-chrome/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-14-chrome
 
 http POST https://api.cykubed.com/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
