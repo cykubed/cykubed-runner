bee3dcd92f096650eb35aa4d5714ccaa2869af03
nick <nickbrook72@gmail.com>
2023-09-22 16:04:58 +0100

    Limit builds to just node 16 and Firefox for now

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.18.3 -f dockerfiles/base/runner/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.18.4 -f dockerfiles/base/runner/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/base-runner
-docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.18.3 -f dockerfiles/generated/full/node-16/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.18.4 -f dockerfiles/generated/full/node-16/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-16
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.18.3 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.18.4 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
 docker push -a us-docker.pkg.dev/cykubed/public/node-16-firefox
 
 http POST https://api.cykubed.com/admin/runner/image -A bearer -a $CYKUBED_API_TOKEN  < dockerfiles/generated/full/cykubed-payload.json
