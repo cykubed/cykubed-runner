16e81a17bdbed59017993f98a665eb9bc4a6517b
nick <nickbrook72@gmail.com>
2023-09-22 15:38:52 +0100

    Limit builds for now

--- dockerfiles/common.py
+++ dockerfiles/common.py
@@ -1,8 +1,8 @@
 import os
 from string import Template
 
-NODE_MAJOR_VERSIONS = ['14', '16', '18', '20']
-BROWSERS = ['chrome', 'edge', 'firefox']
+NODE_MAJOR_VERSIONS = ['16'] #, '18', '20']
+BROWSERS = ['firefox'] #, #'chrome', 'edge']
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 GENERATION_DIR = os.path.join(os.path.dirname(__file__), 'generated')
 BASE_DOCKERFILE = os.path.join(os.path.dirname(__file__), 'base/Dockerfile')
