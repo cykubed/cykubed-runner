f6a0c0719cd2cb439aa4624b694914dc5a6070b8
nick <nickbrook72@gmail.com>
2023-09-26 17:35:42 +0100

    Fixed local build

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.19.4 -f dockerfiles/base/runner/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.19.4 -f dockerfiles/generated/full/node-16/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.19.4 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.19.5 -f dockerfiles/base/runner/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.19.5 -f dockerfiles/generated/full/node-16/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.19.5 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
