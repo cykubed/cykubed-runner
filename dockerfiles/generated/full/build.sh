a4ef8d6cb57d4c70d64bf1c5957a9b524a3959f7
nick <nickbrook72@gmail.com>
2023-10-31 15:23:42 +0000

    new release

--- dockerfiles/generated/full/build.sh
+++ dockerfiles/generated/full/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
-docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.20.14 -f dockerfiles/base/runner/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.20.14 -f dockerfiles/generated/full/node-16/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.20.14 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-chrome:3.20.14 -f dockerfiles/generated/full/node-16-chrome/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-edge:3.20.14 -f dockerfiles/generated/full/node-16-edge/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-16-chrome-edge-firefox:3.20.14 -f dockerfiles/generated/full/node-16-chrome-edge-firefox/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-18:3.20.14 -f dockerfiles/generated/full/node-18/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-18-firefox:3.20.14 -f dockerfiles/generated/full/node-18-firefox/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-18-chrome:3.20.14 -f dockerfiles/generated/full/node-18-chrome/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-18-edge:3.20.14 -f dockerfiles/generated/full/node-18-edge/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-18-chrome-edge-firefox:3.20.14 -f dockerfiles/generated/full/node-18-chrome-edge-firefox/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-20:3.20.14 -f dockerfiles/generated/full/node-20/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-20-firefox:3.20.14 -f dockerfiles/generated/full/node-20-firefox/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-20-chrome:3.20.14 -f dockerfiles/generated/full/node-20-chrome/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-20-edge:3.20.14 -f dockerfiles/generated/full/node-20-edge/Dockerfile .
-docker build -t us-docker.pkg.dev/cykubed/public/node-20-chrome-edge-firefox:3.20.14 -f dockerfiles/generated/full/node-20-chrome-edge-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/base-runner:3.20.15 -f dockerfiles/base/runner/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16:3.20.15 -f dockerfiles/generated/full/node-16/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-firefox:3.20.15 -f dockerfiles/generated/full/node-16-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-chrome:3.20.15 -f dockerfiles/generated/full/node-16-chrome/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-edge:3.20.15 -f dockerfiles/generated/full/node-16-edge/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-16-chrome-edge-firefox:3.20.15 -f dockerfiles/generated/full/node-16-chrome-edge-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-18:3.20.15 -f dockerfiles/generated/full/node-18/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-18-firefox:3.20.15 -f dockerfiles/generated/full/node-18-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-18-chrome:3.20.15 -f dockerfiles/generated/full/node-18-chrome/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-18-edge:3.20.15 -f dockerfiles/generated/full/node-18-edge/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-18-chrome-edge-firefox:3.20.15 -f dockerfiles/generated/full/node-18-chrome-edge-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-20:3.20.15 -f dockerfiles/generated/full/node-20/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-20-firefox:3.20.15 -f dockerfiles/generated/full/node-20-firefox/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-20-chrome:3.20.15 -f dockerfiles/generated/full/node-20-chrome/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-20-edge:3.20.15 -f dockerfiles/generated/full/node-20-edge/Dockerfile .
+docker build -t us-docker.pkg.dev/cykubed/public/node-20-chrome-edge-firefox:3.20.15 -f dockerfiles/generated/full/node-20-chrome-edge-firefox/Dockerfile .
