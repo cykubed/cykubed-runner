22557b73b6a0a43cc8eb73a4c532329ce9aa1adc
nick <nick@myrecovery.ai>
2024-12-30 18:44:13 +0000

    Switching to shell scripts in prep for moving to github actions

--- scripts/build-base-images.sh
+++ scripts/build-base-images.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-REGION=us
+IMAGE="ghcr.io/cykubed/base-runner"
 BASETAG=$(jq -r '.base' dockerfiles/versions.json)
 
 echo "Using base tag $BASETAG"
 
 echo "Build base Cypress image"
-docker build -f dockerfiles/base/cypress/Dockerfile -t $REGION-docker.pkg.dev/cykubed/public/runner/cypress-base:$BASETAG .
+docker build -f dockerfiles/base/cypress/Dockerfile -t $IMAGE-base:$BASETAG .
 
-for nodever in 16 18 20
+for nodever in 20 18 16
 do
 echo "Build base Cypress image for Node $nodever"
-docker build -f dockerfiles/base/node/Dockerfile --build-arg node=$nodever --build-arg basetag=cypress-base:$BASETAG -t $REGION-docker.pkg.dev/cykubed/public/runner/cypress-base-node-$nodever:$BASETAG .
+docker build -f dockerfiles/base/node/Dockerfile --build-arg node=$nodever --build-arg base=$IMAGE-base:$BASETAG -t $IMAGE-cypress-$nodever:$BASETAG .
 
 echo "Build base Playwright image for Node $nodever"
-docker build -f dockerfiles/base/playwright/Dockerfile --build-arg node=$nodever -t $REGION-docker.pkg.dev/cykubed/public/runner/playwright-base-node-$nodever:$BASETAG .
+docker build -f dockerfiles/base/playwright/Dockerfile --build-arg node=$nodever -t $IMAGE-playwright-$nodever:$BASETAG .
+
 done
